7db3c5bd5563c4f3e5769cdca0223de7fbf82d7f
Laura Salamanca Lotero <laurasalamancalotero@gmail.com>
2024-03-27 13:56:35 -0500

    ProductoDisponibleEntradaDto | Se modifica @Pattern

--- src/main/java/com/ebikerrent/alquilerbicicletas/dto/entrada/producto/ProductoDisponibleEntradaDto.java
+++ src/main/java/com/ebikerrent/alquilerbicicletas/dto/entrada/producto/ProductoDisponibleEntradaDto.java
@@ -19,7 +19,8 @@ import java.time.LocalDate;
 public class ProductoDisponibleEntradaDto {
     //@NotNull(message = "El producto no puede ser nulo")
     //@Pattern(regexp = "^\\s*[A-Z0-9][A-Z0-9\\s]*$", message = "El campo debe contener solo letras mayúsculas y números.")
-    @Pattern(regexp = "^\\s*[A-Z]*\\s*$|^\\s+$", message = "El nombre del producto debe estar en mayúsculas")
+    //@Pattern(regexp = "^\\s*[A-Z]*\\s*$|^\\s+$", message = "El nombre del producto debe estar en mayúsculas")
+    @Pattern(regexp = "^[A-Z0-9\\s]*$", message = "El nombre del producto debe estar en mayúsculas y puede contener números")
     @Size(max = 250)
     private String nombreProducto;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
